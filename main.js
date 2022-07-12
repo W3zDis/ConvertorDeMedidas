0ce38a44cfd513d29fb7b68bf00d7d1bdcc1ea3f
luistonicelli@gmail.com <luistonicelli@gmail.com>
2022-07-12 13:13:16 -0300

    a

--- main.js
+++ main.js
@@ -1,17 +1,17 @@
 let GuardarValor = "";
 
 function GuardarV2() {
-  
-  GuardarValor = document.getElementById('valor').value;
-  
-  document.getElementById('Result1').innerHTML = GuardarValor + " Metros pra centímetros: " + GuardarValor*100
-  
-  document.getElementById('Result2').innerHTML = GuardarValor + " Centimetros pra metros: " + GuardarValor/100
-  
-  document.getElementById('Result3').innerHTML = GuardarValor + " Milímetros pra Centímetros: " + GuardarValor/10
 
-  document.getElementById('Result4').innerHTML = GuardarValor + " Quilômetros pra Metros: " + GuardarValor/1000
-  
-  
+  GuardarValor = document.getElementById('valor').value;
+
+  document.getElementById('Result1').innerHTML = GuardarValor + " Metros pra centímetros: " + GuardarValor * 100
+
+  document.getElementById('Result2').innerHTML = GuardarValor + " Centimetros pra metros: " + GuardarValor / 100
+
+  document.getElementById('Result3').innerHTML = GuardarValor + " Milímetros pra Centímetros: " + GuardarValor / 10
+
+  document.getElementById('Result4').innerHTML = GuardarValor + " Quilômetros pra Metros: " + GuardarValor / 1000
+
+
   console.log(GuardarValor)
 }
